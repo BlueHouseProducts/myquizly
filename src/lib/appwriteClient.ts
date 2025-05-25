33a19a9ae5b52bda321f09b49d2ebfcbdcc53a33
Ed Josef <uroemperor256@gmail.com>
2025-05-25 12:15:58 +0100

    Setup appwrite and connect

---
+++ src/lib/appwriteClient.ts
@@ -0,0 +1,12 @@
+import { Client, Account, Databases, Storage } from "appwrite";
+
+const client = new Client()
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_PUBLIC_ENDPOINT!)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, databases, storage };
+export { ID } from 'appwrite';
